87f700ed9fabbaed077c7cad82783fdbe60f32ee
David Dashifen Kees <dashifen@dashifen.com>
2019-09-08 21:02:54 -0400

    better agent identification for factories

--- src/Agents/Collection/Factory/AgentCollectionFactory.php
+++ src/Agents/Collection/Factory/AgentCollectionFactory.php
@@ -3,6 +3,7 @@
 namespace Dashifen\WPHandler\Agents\Collection\Factory;
 
 use Dashifen\WPHandler\Agents\AbstractAgent;
+use Dashifen\WPHandler\Agents\AgentInterface;
 use Dashifen\WPHandler\Handlers\HandlerInterface;
 use Dashifen\WPHandler\Agents\Collection\AgentCollection;
 use Dashifen\WPHandler\Agents\Collection\AgentCollectionException;
@@ -49,16 +50,51 @@ class AgentCollectionFactory implements AgentCollectionFactoryInterface {
    * @throws AgentCollectionFactoryException
    */
   public function registerAgent (string $agent): void {
-    if (!class_exists($agent) || !is_a($agent, AbstractAgent::class)) {
-      $agentNameParts = explode("\\", $agent);
-
+    if (!class_exists($agent)) {
       throw new AgentCollectionFactoryException(
-        sprintf("%s is not an agent", array_pop($agentNameParts)),
-        AgentCollectionFactoryException::NOT_AN_AGENT
+        sprintf("Unknown agent: %s", $this->getAgentShortName($agent)),
+        AgentCollectionFactoryException::UNKNOWN_AGENT
       );
     }
 
-    $this->agentRegistry[] = $agent;
+    // there are three abstract agent classes:  the AbstractAgent, the
+    // AbstractPluginAgent, and the AbstractThemeAgent.  each extend the
+    // Handler of a similar name, not each other.  this is because we need
+    // the Handler's behaviors within each of them, i.e. plugin agents need
+    // to know how plugin Handlers behave.  so, we'll get the parents of
+    // our $agent and see if one of these three is within them.
+
+    $temp = $agent;
+    while ($temp = get_parent_class($temp)) {
+      if (preg_match("/Abstract(?:Theme|Plugin)?Agent/", $temp)) {
+        $this->agentRegistry[] = $agent;
+        return;
+      }
+    }
+
+    // if we didn't return within our loop, that means none of our abstract
+    // agent classes were parents of this one.  therefore, this one's not an
+    // agent.  we've an exception for that...
+
+    throw new AgentCollectionFactoryException(
+      sprintf("%s is not an agent", $this->getAgentShortName($agent)),
+      AgentCollectionFactoryException::NOT_AN_AGENT
+    );
+  }
+
+  /**
+   * getAgentShortName
+   *
+   * Given the fully namespaced name of an Agent, return it's short name,
+   * i.e. it's class name without all the namespacing.
+   *
+   * @param string $agentFullName
+   *
+   * @return string
+   */
+  private function getAgentShortName (string $agentFullName): string {
+    $agentNameParts = explode("\\", $agentFullName);
+    return array_pop($agentNameParts);
   }
 
   /**
@@ -68,6 +104,7 @@ class AgentCollectionFactory implements AgentCollectionFactoryInterface {
    * production as a collection.
    *
    * @param array $agents
+   *
    * @throws AgentCollectionFactoryException
    */
   public function registerAgents (array $agents): void {
@@ -79,6 +116,4 @@ class AgentCollectionFactory implements AgentCollectionFactoryInterface {
       $this->registerAgent($agent);
     }
   }
-
-
 }
