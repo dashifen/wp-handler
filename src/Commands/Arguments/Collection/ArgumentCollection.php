4689ee44e4a031f609118254c4f916e30791897a
David Dashifen Kees <dashifen@dashifen.com>
2021-05-21 10:56:06 -0400

    better description for commands

--- src/Commands/Arguments/Collection/ArgumentCollection.php
+++ src/Commands/Arguments/Collection/ArgumentCollection.php
@@ -78,4 +78,35 @@ class ArgumentCollection extends AbstractCollection implements ArgumentCollectio
     
     parent::offsetSet($offset, $value);
   }
+  
+  /**
+   * getSynopsis
+   *
+   * When we actually want to load our command into the WP CLI, it's not quite
+   * enough to simply return our collection as an array.  Instead we need to
+   * slightly alter things so that the CLI gets its data the way it wants it.
+   *
+   * @return array
+   */
+  public function getSynopsis(): array
+  {
+    foreach ($this->collection as $argument) {
+      
+      // first, we remove any blank indices from our array using array_filter.
+      // then, the CLI wants Boolean values to be listed as strings, so we do
+      // a quick conversion after that.
+      
+      $argumentArray = array_filter($argument->toArray());
+      
+      foreach ($argumentArray as &$value) {
+        if (is_bool($value)) {
+          $value = $value ? 'true' : 'false';
+        }
+      }
+      
+      $synopsis[] = $argumentArray;
+    }
+    
+    return $synopsis ?? [];
+  }
 }
--- src/Commands/Arguments/Collection/ArgumentCollectionInterface.php
+++ src/Commands/Arguments/Collection/ArgumentCollectionInterface.php
@@ -6,5 +6,14 @@ use Dashifen\Collection\CollectionInterface;
 
 interface ArgumentCollectionInterface extends CollectionInterface
 {
-  
+  /**
+   * getSynopsis
+   *
+   * When we actually want to load our command into the WP CLI, it's not quite
+   * enough to simply return our collection as an array.  Instead we need to
+   * slightly alter things so that the CLI gets its data the way it wants it.
+   *
+   * @return array
+   */
+  public function getSynopsis(): array;
 }
